13167b35807b1fd166cbcf43dd9c7b48aa09e46a
Teodor Maxim <tmaxmax@outlook.com>
2020-06-11 19:50:14 +0300

    Implement Metadata API endpoint. Handle unsupported HTTP methods

--- runapi/main.go
+++ runapi/main.go
@@ -17,8 +17,14 @@ func main() {
 
 	r := mux.NewRouter()
 	api := r.PathPrefix("/serverproperties/v1").Subrouter()
+
 	api.HandleFunc("/", serverpropertiesapi.GetAllProperties(prop)).Methods(http.MethodGet)
 	api.HandleFunc("/{key}", serverpropertiesapi.GetProperty(prop)).Methods(http.MethodGet)
+	api.HandleFunc("/meta/", serverpropertiesapi.GetMetadata).Methods(http.MethodGet)
 
-	log.Fatalln(http.ListenAndServe(":8080", r))
+	api.HandleFunc("/", serverpropertiesapi.MethodNotAllowedHandler(http.MethodGet))
+	api.HandleFunc("/{key}", serverpropertiesapi.MethodNotAllowedHandler(http.MethodGet))
+	api.HandleFunc("/meta/", serverpropertiesapi.MethodNotAllowedHandler(http.MethodGet))
+
+	log.Fatalln(http.ListenAndServe(":80", r))
 }
