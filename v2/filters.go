88ffe807c560cbae77dfafbccf29eb8f7c527b92
Teodor Maxim <tmaxmax@outlook.com>
2020-06-19 14:14:33 +0300

    Make filters struct completely unexported

--- v2/filters.go
+++ v2/filters.go
@@ -13,22 +13,22 @@ const (
 	filterUpcoming = "upcoming"
 )
 
-type Filters struct {
+type filters struct {
 	contains  map[string]bool
 	exactName string
 	language  language.Tag
-	sort      map[string]bool
-	types     map[string]bool
+	sortMap   map[string]bool
+	typesMap  map[string]bool
 	upcoming  string
 }
 
-func (f *Filters) Valid(types map[string]struct{}) bool {
-	for key, _ := range f.types {
+func (f *filters) valid(types map[string]struct{}) bool {
+	for key := range f.typesMap {
 		if _, ok := types[key]; !ok {
 			return false
 		}
 	}
-	for key, _ := range f.sort {
+	for key := range f.sortMap {
 		if key != filterName && key != filterType && key != filterUpcoming {
 			return false
 		}
@@ -39,7 +39,7 @@ func (f *Filters) Valid(types map[string]struct{}) bool {
 	return true
 }
 
-func (f *Filters) Filter(p []Property) []Property {
+func (f *filters) filter(p []Property) []Property {
 	ret := make([]Property, 0)
 	if f.exactName != "" {
 		for i := range p {
@@ -50,12 +50,12 @@ func (f *Filters) Filter(p []Property) []Property {
 		}
 		return ret
 	}
-	if f.types == nil && f.contains == nil && f.upcoming == "" {
+	if f.typesMap == nil && f.contains == nil && f.upcoming == "" {
 		return p
 	}
 	for i := range p {
-		if f.types != nil {
-			if accept, ok := f.types[p[i].Type]; !accept || !ok {
+		if f.typesMap != nil {
+			if accept, ok := f.typesMap[p[i].Type]; !accept || !ok {
 				continue
 			}
 		}
@@ -81,8 +81,8 @@ func (f *Filters) Filter(p []Property) []Property {
 	return ret
 }
 
-func (f *Filters) Sort(p []Property) {
-	if order, ok := f.sort[filterName]; ok {
+func (f *filters) sort(p []Property) {
+	if order, ok := f.sortMap[filterName]; ok {
 		if order {
 			sort.Slice(p, func(i, j int) bool {
 				return p[i].Name < p[j].Name
@@ -93,7 +93,7 @@ func (f *Filters) Sort(p []Property) {
 			})
 		}
 	}
-	if order, ok := f.sort[filterType]; ok {
+	if order, ok := f.sortMap[filterType]; ok {
 		if order {
 			sort.Slice(p, func(i, j int) bool {
 				return p[i].Type < p[j].Type
@@ -104,7 +104,7 @@ func (f *Filters) Sort(p []Property) {
 			})
 		}
 	}
-	if order, ok := f.sort[filterUpcoming]; ok {
+	if order, ok := f.sortMap[filterUpcoming]; ok {
 		if order {
 			sort.Slice(p, func(i, j int) bool {
 				return p[i].Upcoming == false && p[j].Upcoming == true
